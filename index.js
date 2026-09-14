7cc562a139a6e2fa281c3c59cd44c37dff78b06b
TaniiiL <50320758@est.ort.edu.ar>
2026-09-14 12:07:53 -0300

    tutorial

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res){
-   res.send("Hello world!");
+app.get('/hello', function(req, res){
+   res.send("holis porfavor que me salga");
+});
+app.post('/hello', function(req, res){
+   res.send("You just called the post method at '/hello'!\n");
 });
 
 app.listen(3000);
