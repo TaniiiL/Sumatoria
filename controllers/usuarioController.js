4e3b8ca1d00e000aa8f31b987307e79676b7f809
TaniiiL <50320758@est.ort.edu.ar>
2026-09-17 12:11:44 -0300

    usuariocontrollerterminado,peobar

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,6 +1,25 @@
+
+import {registrarUsuario} from "../services/usuarioServices.js";
 export function  registrarUsuario(req, res) {
     console.log(req.body);
 
     res.send("Llego la peticion de registrar usuario")
 }
 
+export async function registrarUsuario (req, res){
+    const {nombre, email, password } = req.body;
+
+    if (!nombre || !email ||  !password){
+        return res.status(400).json({mensaje: "Falta completar campos"});
+    }
+    try {
+        const = nuevoUsuario = await  registrarUsuario ({nombre, email, password});
+
+        return res.status(201).json({
+            mensaje: "Usuario registrado con exito"
+            usuario: nuevoUsuario
+        });
+    } catch (error) {
+        return res.status(500).json({mensaje: error.message});
+    }    
+}
--- services/usuarioServices.js
+++ services/usuarioServices.js
@@ -1,5 +1,6 @@
 import {db}   from "../db.js";
 
+export async function registrarUsuario ({nombre, email, password})
 const registrarUsuario = async (req, res) => {
     try {
         
